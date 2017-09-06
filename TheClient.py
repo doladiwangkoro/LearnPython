0bf56ec104bfbf4bdc7cf03e7f545ac104a06ffd
D M Dolaputra <doladiwang@gmail.com>
2017-09-06 12:41:27 +0200

    Socket Programming
    Socket Programming Client and Server

---
+++ TheClient.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  5 19:00:56 2017
+
+@author: ddolaputra
+"""
+
+import socket
+import json
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('145.94.184.28',5650))
+
+
+hello = input("Say hello: ") #client input as strings
+
+#initialized value
+parameter = {
+        'voltage':[3,4,5],
+        'power':[5,6,7]
+        }
+
+
+#the back and forth information transmittal will go on as long as the voltages do not reach 100 
+while parameter['voltage'][0] < 100:
+
+    
+
+    serialized_parameter = json.dumps(parameter) #converting dictionary into strings
+
+
+    #sending part
+    s.send(str.encode(serialized_parameter)) #encoding string into bytes and send it to the server
+    
+    ####The server will do its task in this part###
+    
+    #receiving data after being processed in the server
+    data = s.recv(1024) #receiving data from server
+    serialized_parameter = data.decode('utf-8') #decoding received data from bytes to string
+    
+    
+    parameter = json.loads(serialized_parameter) #converting strings into dictionary
+    
+    print("Receive",parameter) #printing parameter
+    
+    #calculation part : adding 6 for each voltage
+    for i in range(len(parameter['voltage'])):
+        parameter['voltage'][i] = parameter['voltage'][i] + 6
+    
+    print("New",parameter) #printing parameter
+    
+
+s.close()
+print("client has disconnected")
---
+++ TheServer.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  5 18:53:33 2017
+
+@author: ddolaputra
+"""
+
+import socket
+import json
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('',5650))
+print("Listening ...") #waiting for a connection
+
+s.listen(1)
+
+connection, address = s.accept() #accepting connection from a client 
+print("Connected to the client", address)
+
+### The client is taking information and sending it here ###
+
+while True:
+    dat = connection.recv(1024) #receiving bytes from client as data
+    data = dat.decode('utf-8') #decoding received data from bytes to string
+    
+    parameter = json.loads(data) #converting received strings (after being converted from bytes) into dictionary
+    
+    print("Recieved from client:",parameter) #printing data
+    
+    #calculation part : adding 10 for each voltage
+    for i in range(len(parameter['voltage'])):
+        parameter['voltage'][i] = parameter['voltage'][i] + 10 
+    
+    
+    
+    if not data:
+        break #break the loop when there's no data sent
+
+    print("Will be sent to client:",parameter) #printing data
+    
+    
+    serialized_parameter = json.dumps(parameter) #converting dictionary into strings for being transmitted to the client
+    
+    #transmittal
+    connection.send(str.encode(serialized_parameter)) #encoding data from string to bytes
+    
+connection.close()
+
